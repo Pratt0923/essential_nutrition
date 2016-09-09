03f7e830040189ada55e501f77ea279fc2aebedb
Alyssa Pratt <snubly1@gmail.com>
2016-09-09 11:11:24 -0400

    changed layout of everything because this looks better

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){$("ul li > a").click(function(){$("ul li > a").removeAttr("class");$(this).addClass("home")});$("ul li a").click(function(){var e=$(this.hash);$("body,html").animate({scrollTop:e.offset().top},700);return false});$(window).scroll(function(){if($(this).scrollTop()>500){$(".top").fadeIn()}else{$(".top").fadeOut()}});$(".top").click(function(){$("body,html").animate({scrollTop:0},500);return false})})
+/*
+	* Design by : Mahmoud Abd Alkarim ^_^
+	* Twitter   : AbdAlkarimTweet
+	* Contact   : AbdAlkarim.mail@gmail.com
+*/
